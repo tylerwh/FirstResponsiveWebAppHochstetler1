c137f0d3107803ee0555efdd6d8cdc018077c9f8
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2021-05-31 13:49:07 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppHochstetler.Models;
+
+namespace FirstResponsiveWebAppHochstetler.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                int age = (int)model.AgeToday();
+                ViewBag.Message = model.FirstName + ", you are " + age + " years old today.";
+            }
+            else
+            {
+                ViewBag.Message = "";
+            }
+            
+            return View(model);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppHochstetler.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Please enter your first name.")]
+        public String FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last name.")]
+        public String LastName { get; set; }
+        [Required(ErrorMessage = "Please select a date for your birthday.")]
+        [DataType(DataType.DateTime)]
+        public DateTime DateOfBirth { get; set; }
+        public int? AgeToday()
+        {
+            int currentAge = DateTime.Now.Year - DateOfBirth.Year;
+            if (DateTime.Now.DayOfYear < DateOfBirth.DayOfYear)
+            {
+                currentAge -= 1;
+            }
+            return currentAge;
+        }
+    }
+}
