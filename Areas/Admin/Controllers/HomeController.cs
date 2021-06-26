a6356f8789a5fbf76797db5d7991fa662b74253f
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2021-06-26 08:25:32 -0500

    Added Areas folder and routing

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppHochstetler.Models;
+
+namespace FirstResponsiveWebAppHochstetler.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("[area]/[controller]/{id?}")]
+        public IActionResult Index()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        [Route("[area]/[controller]/{id?}")]
+        public IActionResult Index(Person model)
+        {
+            if (ModelState.IsValid)
+            {
+                int age = (int)model.AgeToday();
+                ViewBag.Message = model.FirstName + ", you are " + age + " years old today.";
+            }
+            else
+            {
+                ViewBag.Message = "";
+            }
+            
+            return View(model);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -39,6 +39,10 @@ namespace FirstResponsiveWebAppHochstetler
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    name: "admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
